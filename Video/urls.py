ed5d97ae758ca0eb3aa28a29ac1c2fe34a164618
wkdthf21 <wkdthf21@naver.com>
2018-11-04 15:56:00 +0900

    add Video MainTestPage urls

--- Video/urls.py
+++ Video/urls.py
@@ -1,6 +1,8 @@
+from django.urls import path
 from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path('/list', views.ViedoList, name = 'ViedoList'),
+    # path('list/', views.VideoList, name = 'ViedoList'),
+    path('mainTest/', views.Test_VideoMain, name = 'Test_VideoMain'),
 ]
