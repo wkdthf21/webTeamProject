105da3a6fd433ebb142db12cef37450cb5287fe2
wkdthf21 <wkdthf21@naver.com>
2018-11-04 16:00:45 +0900

    change entire urls and add applications in setting.py

--- webTeamProject/settings.py
+++ webTeamProject/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Video',
+    'accounts',
+    'classroom',
+    'Main',
+    'News',
+    'sugang',
+    'Vocabulary',
+    'word',
 ]
 
 MIDDLEWARE = [
--- webTeamProject/urls.py
+++ webTeamProject/urls.py
@@ -18,12 +18,12 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('accounts.urls')),
-    path('classroom/', include('classroom.urls')),
-    path('Main/', include('Main.urls')),
-    path('News/', include('News.urls')),
-    path('sugang/', include('sugang.urls')),
-    path('Video/', include('Video.urls')),
-    path('Vocabulary/', include('Vocabulary.urls')),
-    path('word/', include('word.urls')),
+    #path('account/', include('accounts.urls')),
+    #path('classroom/', include('classroom.urls')),
+    #path('Main/', include('Main.urls')),
+    #path('News/', include('News.urls')),
+    #path('sugang/', include('sugang.urls')),
+    #path('Video/', include('Video.urls')),
+    #path('Vocabulary/', include('Vocabulary.urls')),
+    #path('word/', include('word.urls')),
 ]
