ada747070d28c2c74f3a00074e7e24176456f001
wkdthf21 <wkdthf21@naver.com>
2018-12-06 23:34:00 +0900

    git add MainView

--- Main/views.py
+++ Main/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, HttpResponseRedirect, redirect
 
 # Create your views here.
+
+def Main(request):
+
+    # 로그인을 한 상태
+    if 'userId' in request.session :
+        userId = request.session['userId']
+        return render(request, 'main.html', {'userId' : userId})
+
+   # 로그인을 하지 않은 상태
+    else:
+        redirect_to = reverse('Login')
+        return HttpResponseRedirect(redirect_to)
