867525e1b5d02d1bba19266b1f6839913632c672
tyg04308 <tyg04308@naver.com>
2018-12-08 01:31:10 +0900

    make word models

--- word/models.py
+++ word/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Word(models.Model):
+    word_id=models.IntegerField()
+    word_spell=models.CharField()
+    word_mean=models.CharField()
+    word_update_date=DateTimeField(default=timezone.now)
+    u_id=ForeignKey('account.user', on_delete=models.CASCADE)
--- wordTest/models.py
+++ wordTest/models.py
@@ -6,12 +6,11 @@ from django.utils import timezone
 class Test(models.Model):
     test_id = models.IntegerField()
     test_date = models.DateTimeField(default=timezone.now)
-#    u_id = models.ForeignKey('account.user', on_delete=models.CASCADE) 
+#    u_id = models.ForeignKey('account.user', on_delete=models.CASCADE)
     u_id = models.IntegerField()
     score = models.IntegerField()
 
 
 class Review(models.Model):
     test_id = models.ForeignKey('wordTest.Test', on_delete=models.CASCADE)
-#    wrong_word_id = models.ForeignKey('word.Word', on_delete=models.CASCADE)
-    wrong_word_id = models.IntegerField()
+    wrong_word_id = models.ForeignKey('word.Word', on_delete=models.CASCADE)
