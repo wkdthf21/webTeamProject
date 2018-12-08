8b9b56b8ceb48390b360b2c0c8d66d02a2c443cf
tyg04308 <tyg04308@naver.com>
2018-12-09 03:00:40 +0900

    make init link

--- webTeamProject/urls.py
+++ webTeamProject/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #path('', include('Main.urls')),
     #path('account/', include('accounts.urls')),
     #path('classroom/', include('classroom.urls')),
     #path('Main/', include('Main.urls')),
