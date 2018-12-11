a91aa1c579dfa58c77ad6812578c9d47fa00bfb9
wkdthf21 <wkdthf21@naver.com>
2018-12-11 17:06:16 +0900

    add Caption edit urls

--- Video/urls.py
+++ Video/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('add/', views.addVideo, name='addVideo'),
     path('process_addVideo', views.process_addVideo, name = 'process_addVideo'),
     url(r'^main/(?P<video_id>\d+)/$', views.VideoMain),
+    url(r'^main/caption/(?P<video_id>\d+)/$', views.VideoCaption),
     path('mainTest/', views.Test_VideoMain, name = 'Test_VideoMain'),
     ### YouTube ###
 #    path('index/', views.index, name='index'),
