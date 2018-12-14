85d45eb7da668bb39eaa14bab086f1881b894fe5
wkdthf21 <wkdthf21@naver.com>
2018-12-14 21:15:17 +0900

    add Login and Logout css

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,8 +5,16 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
+        help_texts = {
+            'username': None,
+            'email': None,
+        }
 
 class LoginForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'password'] # 로그인 시에는 유저이름과 비밀번호만 입력 받는다.
+
+        help_texts = {
+            'username': None,
+        }
